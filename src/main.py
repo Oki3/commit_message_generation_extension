3a24b7010c2ab38e0ea44de80a92ba20df71c388
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2024-12-12 17:51:47 +0100

    Update requirements for llama-cpp-python and refactor output writing for cleaner main function

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import os
 import random
 import time
 
-from langchain_community.chat_models import ChatDeepInfra
-
 from mistral_wrapper_llama_cpp import LlamaMistralWrapper
 import pandas as pd
 from dotenv import load_dotenv
@@ -13,10 +11,9 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_ollama import ChatOllama
 
 from extract_diff import import_dataset
-from src.phi_mini import PhiMiniWrapper
+from phi_mini import PhiMiniWrapper
 
 load_dotenv()
-
 
 
 def call_model_sync(model, messages):
@@ -24,19 +21,21 @@ def call_model_sync(model, messages):
     llm = LLMS[model]
     return llm.invoke(messages) if model == "mistral" else llm.invoke(messages).content
 
-def call_model_with_prompt(model,prompt):
+
+def call_model_with_prompt(model, prompt):
     llm = LLMS[model]
     return llm.invoke(prompt)
 
 
-def prepare_prompt(diff,model_name):
-    if model_name=='mistral':
-      return f"Summarize this git diff into a useful, 10 words commit message{diff}\n\nCommit message:"
-    elif model_name=='phi_mini':
-      return (f"Summarize this git diff into a useful, 10 words commit message{diff}. "
-              f"You should only output the commit message and no extra description.\n\nCommit message:")
+def prepare_prompt(diff, model_name):
+    if model_name == 'mistral':
+        return f"Summarize this git diff into a useful, 10 words commit message{diff}\n\nCommit message:"
+    elif model_name == 'phi_mini':
+        return (f"Summarize this git diff into a useful, 10 words commit message{diff}. "
+                f"You should only output the commit message and no extra description.\n\nCommit message:")
 
-def generate_commit_message(prompt,model_name):
+
+def generate_commit_message(prompt, model_name):
     return call_model_with_prompt(model_name, prompt)
 
 
@@ -46,10 +45,10 @@ def process_dataset_quantized_instruct(model_name, dataset, csv_writer):
         # print(diff)
         original_message = item['message']
         print(f"Original message : {original_message}")
-        prompt=prepare_prompt(diff,model_name)
-        commit_message=generate_commit_message(prompt,model_name)
+        prompt = prepare_prompt(diff, model_name)
+        commit_message = generate_commit_message(prompt, model_name)
         print(f"Commit message : {commit_message}")
-        csv_writer.writerow([original_message,commit_message])
+        csv_writer.writerow([original_message, commit_message])
 
 
 def process_dataset_chatbot(model_name, dataset, csv_writer):
@@ -60,44 +59,15 @@ def process_dataset_chatbot(model_name, dataset, csv_writer):
         original_message = item['message']
         message = [
             SystemMessage(content="Be a helpful assistant with knowledge of git message conventions."),
-            HumanMessage(content=f"Summarize this git diff {diff}. into a useful, 10 words commit message. It is very important that you only provide the final output without any additional comments or remarks."),
+            HumanMessage(
+                content=f"Summarize this git diff {diff}. into a useful, 10 words commit message. It is very important that you only provide the final output without any additional comments or remarks."),
         ]
         model_output = call_model_sync(model_name, message)
         csv_writer.writerow([original_message, model_output])
 
 
-if __name__ == "__main__":
-    print("Compiling LLMs")
-    LLMS = {
-         'deepinfra': ChatDeepInfra(model="databricks/dbrx-instruct", temperature=0),
-         'codellama': ChatOllama(model="codellama", base_url="http://localhost:11434"),
-         'mistral':LlamaMistralWrapper(),
-         'phi_mini':PhiMiniWrapper()
-    }
-    print("Creating Output directory")
-    # Create output directory
-    output_dir = "output"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    print("Separating datasets")
-    # Load dataset
-    train_dataset, validation_dataset, test_dataset = import_dataset("Maxscha/commitbench")
-    print(train_dataset.shape)
-    print("Receiving model Names")
-    model_name = input("Enter the model name: ")
-
-    # Handle invalid input
-    if model_name not in LLMS:
-        print(f"Model {model_name} not found.")
-        exit(1)
-
-    # Prepare subset
-    dataset_to_process = (
-        train_dataset.select(random.sample(range(len(train_dataset)), 10))
-        if model_name in ["mistral" ,"phi_mini"] else train_dataset
-    )
-
-    # Write output to CSV
+def save_output():
+    global start_time, end_time
     output_file = os.path.join(output_dir, "output.csv")
     with open(output_file, mode="w", newline='', encoding="utf-8") as csvfile:
         csv_writer = csv.writer(csvfile)
@@ -110,10 +80,46 @@ if __name__ == "__main__":
             process_dataset_chatbot(model_name, dataset_to_process, csv_writer)
         end_time = time.perf_counter()
         print(f"End Time: {end_time}")
-        print(f"Total Time taken for executing train set is {end_time-start_time} ")
-
+        print(f"Total Time taken for executing train set is {end_time - start_time} ")
     # Display CSV content
     print(pd.read_csv(output_file))
+
+
+if __name__ == "__main__":
+    print("Compiling LLMs")
+    LLMS = {
+        'deepinfra': ChatDeepInfra(model="databricks/dbrx-instruct", temperature=0),
+        'codellama': ChatOllama(model="codellama", base_url="http://localhost:11434"),
+        'mistral': LlamaMistralWrapper(),
+        'phi_mini': PhiMiniWrapper()
+    }
+    # Create output directory
+    print("Creating Output directory")
+    output_dir = "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Load dataset
+    print("Separating datasets")
+    train_dataset, validation_dataset, test_dataset = import_dataset("Maxscha/commitbench")
+    print(train_dataset.shape)
+
+    print("Receiving model Names")
+    model_name = input("Enter the model name: ")
+
+    # Handle invalid input
+    if model_name not in LLMS:
+        print(f"Model {model_name} not found.")
+        exit(1)
+
+    # Prepare subset
+    dataset_to_process = (
+        train_dataset.select(random.sample(range(len(train_dataset)), 10))
+    )
+
+    # Write output to CSV
+    save_output()
+
     if 'phi_mini' in LLMS:
         LLMS['phi_mini'].close()
     del LLMS[model_name]
