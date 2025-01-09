cca70d3d5ad2f801ecc907477b3caca6b0d72c9c
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2025-01-09 15:02:09 +0100

    Change to generate and add some options

--- src/main.py
+++ src/main.py
@@ -8,13 +8,10 @@ import time
 class Model:
     name: str = ""
 
-    def run(self, prompt: str) -> str:
-        response = ollama.chat(model=self.name, messages=[{
-            "role": "user",
-            "content": prompt
-        }])
+    def run(self, prompt: str, temperature: float) -> str:
+        response = ollama.generate(model=self.name, prompt=prompt, options={"temperature": temperature})
 
-        return response.message.content.strip()
+        return response.response
     
     def check_installed(self) -> bool:
         try:
@@ -27,39 +24,37 @@ class Model:
 class MistralModel(Model):
     name: str = "mistral"
 
-    def run(self, prompt: str) -> str:
-        return super().run(prompt)
-
 class CodellamaModel(Model):
     name: str = "codellama"
-
-    def run(self, prompt: str) -> str:
-        return super().run(prompt)
     
 class Phi35Model(Model):
     name: str = "phi3.5"
 
-    def run(self, prompt: str) -> str:
-        return super().run(prompt)
-
 class Experiment:
     model: Model
-    size: int
+    input_size: int
+    process_amount: int
     prompt: str
     input_file: str
     output_file: str
     input_df: DataFrame|None = None
     output_df: DataFrame|None = None
     start_time: float = 0
+    workers: int
 
-    def __init__(self, model: Model, size: int, prompt: str, input_folder: str, output_folder: str):
+    def __init__(self, model: Model, input_size: int, process_amount: int, prompt: str, input_folder: str, output_folder: str, workers: int):
         self.model = model
-        self.size = size
+        self.input_size = input_size
+        self.process_amount = process_amount
         self.prompt = prompt
-        self.input_file = f"{input_folder}/{model.name}_{1000}_{prompt}.csv"
-        self.output_file = f"{output_folder}/{model.name}_{size}_{prompt}.csv"
+        self.workers = workers
+        self.input_file = f"{input_folder}/{model.name}_{input_size}_{prompt}.csv"
+        self.output_file = f"{output_folder}/{model.name}_{process_amount}_{prompt}.csv"
         
         self.output_df = DataFrame(columns=["hash", "project", "true_message", "generated_message"])
+
+        if self.process_amount > self.input_size:
+            self.process_amount = self.input_size
 
     def check_installed(self):
         if not self.model.check_installed():
@@ -71,15 +66,11 @@ class Experiment:
     def save_output(self):
         self.output_df.to_csv(self.output_file, index=False)
 
-    def process_item(self, index: int):
+    def process_item(self, index: int, temperature: float) -> dict:
         item = self.input_df.iloc[index]
         prompt = item['prompt']
 
-        print(f"{self.model.name}/{self.prompt} - {item['hash'][:7]} - running")
-
-        generated_message = self.model.run(prompt)
-
-        print(f"{self.model.name}/{self.prompt} - {item['hash'][:7]} - finished")
+        generated_message = self.model.run(prompt, temperature)
 
         return {
             "hash": item['hash'],
@@ -115,9 +106,9 @@ class Experiment:
     def start(self):
         self.start_time = time.time()
 
-    def run_parallel(self):
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            futures = {executor.submit(self.process_item, i): i for i in range(self.size)}
+    def run_parallel(self, temperature: float):
+        with ThreadPoolExecutor(max_workers=self.workers) as executor:
+            futures = {executor.submit(self.process_item, i, temperature): i for i in range(self.process_amount)}
             total = len(futures)
             completed = 0
 
@@ -141,14 +132,14 @@ class Experiment:
                 executor.shutdown(wait=False, cancel_futures=True)
                 raise KeyboardInterrupt()
 
-    def run(self):
+    def run(self, temperature: float):
         self.start()
 
-        for i in range(self.size):
+        for i in range(self.process_amount):
             try:
-                result = self.process_item(i)
+                result = self.process_item(i, temperature)
                 self.append_result(i, result)
-                self.print_progress(i+1, self.size)
+                self.print_progress(i+1, self.process_amount)
             except Exception as e:
                 print(f"Error processing item {i}: {e}")
                 self.append_error(i)
@@ -158,16 +149,19 @@ MODELS = {
     "codellama": CodellamaModel(),
     "phi3.5": Phi35Model()
 }
-EXPERIMENTS = ["baseline", "few_shot", "cot"]
+EXPERIMENTS = ["baseline", "fewshot", "cot"]
 
-parser = argparse.ArgumentParser(description="Run a model with a given prompt.")
+parser = argparse.ArgumentParser(description="Run one of the experiments with the specified model.")
 
 parser.add_argument("--model", type=str, default="mistral", choices=MODELS.keys(), help="The model to use.")
 parser.add_argument("--prompt", type=str, default="baseline", choices=EXPERIMENTS, help="The prompt to use.")
-parser.add_argument("--size", type=int, default=1000, help="The number of items to process.")
+parser.add_argument("--input_size", type=int, default=1000, help="The size of te input.")
+parser.add_argument("--process_amount", type=int, default=1000, help="The number of items to process.")
 parser.add_argument("--sequential", action="store_true", help="Run the experiment sequentially instead of in parallel.")
 parser.add_argument("--input_folder", type=str, default="./input", help="The folder containing the input files.")
 parser.add_argument("--output_folder", type=str, default="./output", help="The folder to save the output files.")
+parser.add_argument("--temperature", type=float, default=0.7, help="The temperature for the model generation.")
+parser.add_argument("--workers", type=int, default=5, help="The number of workers to use for parallel processing.")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -175,20 +169,23 @@ if __name__ == "__main__":
     model_name = args.model
     model = MODELS[model_name]
     prompt = args.prompt
-    size = args.size
+    input_size = args.input_size
+    process_amount = args.process_amount
     sequential = args.sequential
     input_folder = args.input_folder
     output_folder = args.output_folder
+    temperature = args.temperature
+    workers = args.workers
 
     os.makedirs(output_folder, exist_ok=True)
 
-    experiment = Experiment(model, size, prompt, input_folder, output_folder)
+    experiment = Experiment(model, input_size, process_amount, prompt, input_folder, output_folder, workers)
     experiment.check_installed()
     experiment.read_input()
     
     if sequential:
-        experiment.run()
+        experiment.run(temperature)
     else:
-        experiment.run_parallel()
+        experiment.run_parallel(temperature)
     
     experiment.save_output()
